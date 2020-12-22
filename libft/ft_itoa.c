16d745c562beb9dfaa70a0e835733647b7c0aa9f
Kim, Gyeonhyun <joegole@gmail.com>
2020-12-22 17:26:15 +0900

    add part2

--- libft/ft_itoa.c
+++ libft/ft_itoa.c
@@ -3,16 +3,67 @@
 /*                                                        :::      ::::::::   */
 /*   ft_itoa.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gkim <gkim@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: gkim <gkim@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/22 14:31:17 by gkim              #+#    #+#             */
-/*   Updated: 2020/12/22 14:34:12 by gkim             ###   ########.fr       */
+/*   Updated: 2020/12/22 17:00:55 by gkim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_itoa(int n)
+static size_t	chk_digit(int n)
 {
-	
+	size_t	digit;
+
+	digit = 0;
+	if (n == 0)
+		return (1);
+	if (n < 0)
+		digit++;
+	while (n)
+	{
+		n = n / 10;
+		digit++;
+	}
+	return (digit);
+}
+
+static char	*ft_convert(unsigned int n,	char *num)
+{
+	int i;
+
+	i = (int)ft_strlen(num) - 1;
+	while (n)
+	{
+		num[i] = (n % 10) + '0';
+		n = n / 10;
+		i--;
+	}
+	return (num);
+}
+
+char		*ft_itoa(int n)
+{
+	unsigned int num;
+	char		*result;
+	size_t		digit;
+
+	digit = chk_digit(n);
+	result = (char *)malloc(digit + 1);
+	result = (char *)ft_memset(result, 1, digit);
+	result[digit] = 0;
+	if (n == 0)
+	{
+		result[0] = '0';
+		return (result);
+	}
+	if (n < 0)
+	{
+		num = (unsigned int)(n * -1);
+		result[0] = '-';
+	}
+	else if (n > 0)
+		num = (unsigned int)n;
+	return (ft_convert(num, result));
 }
--- libft/main.c
+++ libft/main.c
@@ -3,12 +3,6 @@
 
 int		main(void)
 {
-	char	**test;
-	int		i = 0;
-	test = ft_split("12345 6789 10 11 112", ' ');
-	while (test[i] != 0)
-	{
-		printf("%s\n", test[i]);
-		i++;
-	}
+	printf("%s\n", ft_itoa(0));
+	return (0);
 }
