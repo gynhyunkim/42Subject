03f41b2a9f6f79006e24652e994e7d4f3da6224f
Kim, Gyeonhyun <joegole@gmail.com>
2021-01-19 17:26:18 +0900

    main

--- get_next_line/main.c
+++ get_next_line/main.c
@@ -2,14 +2,83 @@
 #include <stdio.h>
 #include <fcntl.h>
 
-int     main(int argc, char **argv)
+int		main(int argc, char **argv)
 {
-    int fd = open("test.txt",O_RDONLY);
-    char *line;
+	char	*line;
+	int		ret;
+	int		fd;
 
-    while (get_next_line(fd, &line) > 0)
-    {
-        get_next_line(fd, &line);
-        printf("%s\n", line);
-    }
+	if (argc == 0)
+		return(0);
+//	fd = 0;
+
+	fd = open(argv[1], O_RDONLY);
+	int fd2 = open(argv[2], O_RDONLY);
+	int fd3 = open(argv[3], O_RDONLY);
+
+
+/*	while ((ret = get_next_line(fd, &line)) > 0)
+	{
+		printf("get_next_line : %s\n", line);
+		printf("return value : %d\n\n", ret);
+		free(line);
+	}
+	printf("get_next_line : %s\n", line);
+	printf("return value : %d\n", ret);
+	free(line);
+	char *line2;
+	ret = get_next_line(fd, &line2);
+	printf("get_next_line : %s\n", line2);
+	printf("return value : %d\n", ret);
+	free(line2);
+	char *line3;
+	ret = get_next_line(fd, &line3);
+	printf("get_next_line : %s\n", line3);
+	printf("return value : %d\n", ret);
+	free(line3);
+*/
+
+	ret = get_next_line(fd, &line);
+	printf("get_next_line : %s\n", line);
+	printf("return value : %d\n\n", ret);
+	free(line);
+
+	ret = get_next_line(fd2, &line);
+	printf("get_next_line : %s\n", line);
+	printf("return value : %d\n\n", ret);
+	free(line);
+
+	ret = get_next_line(fd3, &line);
+	printf("get_next_line : %s\n", line);
+	printf("return value : %d\n\n", ret);
+	free(line);
+
+	ret = get_next_line(fd, &line);
+	printf("get_next_line : %s\n", line);
+	printf("return value : %d\n\n", ret);
+	free(line);
+
+	ret = get_next_line(fd2, &line);
+	printf("get_next_line : %s\n", line);
+	printf("return value : %d\n\n", ret);
+	free(line);
+
+	ret = get_next_line(fd3, &line);
+	printf("get_next_line : %s\n", line);
+	printf("return value : %d\n\n", ret);
+	free(line);
+
+	ret = get_next_line(fd, &line);
+	printf("get_next_line : %s\n", line);
+	printf("return value : %d\n", ret);
+	free(line);
+
+	ret = get_next_line(fd, &line);
+	printf("get_next_line : %s\n", line);
+	printf("return value : %d\n", ret);
+	free(line);
+	while(1)
+		;
+
+	return (0);
 }
