99b46b0fc9959f3a0c245db1709d67f0c2f7bf17
gynhyunkim <gyeonghyun.kimm@gmail.com>
2021-09-23 18:40:28 +0900

    update: norm에 맞춰서 수정(50%)

--- minitalk/minitalk.c
+++ minitalk/minitalk.c
@@ -6,46 +6,53 @@
 /*   By: gkim <gkim@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/22 17:45:13 by gkim              #+#    #+#             */
-/*   Updated: 2021/08/17 17:13:31 by gkim             ###   ########.fr       */
+/*   Updated: 2021/09/23 18:39:56 by gkim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-void	convert(char c, char *b)
+void	send_signal(int pid, char *bit)
+{
+	int	i;
+
+	i = 0;
+	while (i < 8)
+	{
+		kill(pid, SIGUSR1 + bit[i]);
+		usleep(100);
+		i++;
+	}	
+}
+
+void	char_to_bit(char c, char *c_bit)
 {
 	int	i;
 
 	i = 7;
 	while (i >= 0)
 	{
-		b[7 - i] = (c >> i) & 1;
+		c_bit[7 - i] = (c >> i) & 1;
 		i--;
 	}
 }
 
 int	main(int argc, char *argv[])
 {
-	int	pid;
-	int	cnt;
-	char	c[8];
+	int		pid;
 	char	*str;
+	char	bit[8];
 
 	if (argc != 3)
 		write(0, "ERROR!\n", 7);
 	pid = ft_atoi(argv[1]);
-	cnt = 0;
 	str = argv[2];
-	while (*str) {
-		convert(*str, c);
-		for (int i = 0; i < 8; i++) {
-				kill(pid, SIGUSR1 + c[i]);
-				usleep(100);
-		}
+	while (*str)
+	{
+		char_to_bit(*str, bit);
+		send_signal(pid, bit);
 		str++;
 	}
-	for (int i = 0; i < 8; i++) {
-		kill(pid, SIGUSR1);
-		usleep(100);
-	}
+	char_to_bit(*str, bit);
+	send_signal(pid, bit);
 }
