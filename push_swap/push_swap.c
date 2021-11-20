9bbd0814b063c6c97795222c7a408ce588ff9644
gynhyunkim <gyeonghyun.kimm@gmail.com>
2021-11-21 01:03:18 +0900

    update : libft/ norm 확인, zsh 버전 추가

--- push_swap/push_swap.c
+++ push_swap/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: gkim <gkim@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/24 14:36:04 by gkim              #+#    #+#             */
-/*   Updated: 2021/11/21 00:14:37 by gkim             ###   ########.fr       */
+/*   Updated: 2021/11/21 01:02:00 by gkim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,10 @@ static int	check_input(t_obj *o, int *array, int argc, char **argv)
 {
 	int	i;
 
-	i = 1;
+	i = 0;
 	while (i < argc)
 	{
-		if (insert_array(argv[i], array, i - 1))
+		if (insert_array(argv[i], array, i))
 			return (1);
 		add_back(init_node(ft_atoi(argv[i])), o->stack[0]);
 		i++;
@@ -27,30 +27,56 @@ static int	check_input(t_obj *o, int *array, int argc, char **argv)
 	return (0);
 }
 
+static void	free_argv(char **argv)
+{
+	int	i;
+
+	i = 0;
+	while (argv[i])
+	{
+		free(argv[i]);
+		argv[i++] = NULL;
+	}
+	free(argv);
+}
+
+static int	length_argv(char **argv)
+{
+	int	i;
+
+	i = 0;
+	if (!argv)
+		return (0);
+	while (argv[i])
+		i++;
+	return (i);
+}
+
 int	main(int argc, char **argv)
 {
-	t_node	*node;
 	t_obj	*o;
 	int		*array;
 
 	if (argc > 1)
 	{
-		array = malloc(sizeof(int) * (argc - 1));
+		argv = ft_split(argv[1], ' ');
+		argc = length_argv(argv);
+		array = malloc(sizeof(int) * (argc));
 		o = init_stack();
-		if (!check_input(o, array, argc, argv) && !is_sorted(o->stack[0]))
+		if (argv && !check_input(o, array, argc, argv)
+			&& !is_sorted(o->stack[0]))
 		{
-			if (argc == 4)
+			arr_quick_sort(array, 0, argc - 1);
+			if (argc == 3)
 				three_sort(o, 0);
-			else if (argc == 6)
+			else if (argc == 5)
 				five_param(o);
 			else
-			{
-				arr_quick_sort(array, 0, argc - 2);
-				a_to_b(o, array, 0, argc - 1);
-			}
+				a_to_b(o, array, 0, argc);
 		}
 		cmd_parsing(o->cmd);
 		free_stack(o);
 		free(array);
+		free_argv(argv);
 	}
 }
